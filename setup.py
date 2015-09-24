94663a554006be021344718f97b3f09e282ebc61
Tom Hirst-Dunton <tom@labminds.co.uk>
2015-09-24 16:15:48 +0100

    Mark README file in setup.cfg.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
 
 setup(
     name='python-html-assert',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     author='Robert Cox',
